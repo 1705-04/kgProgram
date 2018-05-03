2506561ec737bc3bc88abd2dcb6c6a73cf61e226
YeOuBa <17601062610@163.com>
2018-05-03 14:14:40 +0800

    测试

---
+++ src/main/scala/task/analysislog/AnalysisLogTask.scala
@@ -0,0 +1,14 @@
+package task.analysislog
+
+/**
+  * Created by Administrator on 2018/5/3.
+  */
+object AnalysisLogTask {
+
+  /*
+  整合
+   */
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ src/main/scala/utils/Utils.scala
@@ -0,0 +1,8 @@
+package utils
+
+/**
+  * Created by Administrator on 2018/5/3.
+  */
+object Utils {
+
+}
